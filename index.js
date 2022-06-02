04fd27a0a5dfcd553e886d2dabf43e1893e870da
karthikgopalk <riokarthy@gmail.com>
2022-06-02 09:09:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+let arr = ['Nemo'];
+function findMemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'Nemo') console.log('Nemo Found');
+  }
+}
+findMemo(arr);
